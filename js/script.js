f40c0d9e6aaf2cf6787df0daad7fa55e4eac657e
Tarcisio Freire Jr <tarcisiofreire@MacBook-Pro-de-Tarcisio.local>
2020-05-04 21:28:49 -0300

    Criação de script básico apenas para demonstrar o botão do chat.

---
+++ js/script.js
@@ -0,0 +1,5 @@
+let btContact = document.querySelector('.bt-contact');
+
+btContact.addEventListener("click", function(){
+    document.getElementById("main-view").classList.toggle('chat-activated');
+});
