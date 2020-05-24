090d5b535e7ff18d7205f633f68aa841c35d7b41
Tarcisio Freire Jr <tarcisiofreire@MacBook-Pro-de-Tarcisio.local>
2020-05-24 08:27:02 -0300

    Pequenos ajustes nas transições

--- js/script.js
+++ js/script.js
@@ -1,5 +1,11 @@
 let btContact = document.querySelector('.bt-contact');
 
 btContact.addEventListener("click", function(){
-    document.getElementById("main-view").classList.toggle('chat-activated');
+    let listClasses = document.getElementById("main-view").classList;
+    if(listClasses.length == 0){
+        listClasses.add('chat-activated');
+    }else{
+        listClasses.toggle('chat-activated');
+        listClasses.toggle('chat-deactivated');
+    };
 });
